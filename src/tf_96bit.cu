bf96a6c9eb81cd4d45114f4dd6a568cfe3fa79c7
Danilo Nitsche <danilo.nitsche@gmx.de>
2024-04-24 23:54:36 +0200

    Add mod_128_96 (copied from mod_192_96)

--- src/tf_96bit.cu
+++ src/tf_96bit.cu
@@ -51,6 +51,253 @@ __device__ static void mul96_96(int192 *res, int96 a, unsigned int b)
 }
 
 #ifndef DEBUG_GPU_MATH
+__device__ static void mod_128_96(int96 *res, int192 q, int96 n, float nf)
+#else
+__device__ static void mod_128_96(int96 *res, int192 q, int96 n, float nf, unsigned int *modbasecase_debug)
+#endif
+/* res = q mod n */
+{
+  float qf;
+  unsigned int qi;
+  int192 nn;
+  // int96 tmp96;
+
+/********** Step 1, Offset 2^75 (2*32 + 11) **********/
+/*
+the 75 bit kernel has only one difference: the first iteration of the
+division will be skipped
+*/
+#if !defined(SHORTCUT_75BIT) && !defined(SHORTCUT_64BIT)
+  qf= __uint2float_rn(q.d5);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d4);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d3);
+  qf*= 2097152.0f;
+
+  qi=__float2uint_rz(qf*nf);
+
+  MODBASECASE_QI_ERROR(1<<22, 1, qi, 0);
+
+
+// nn = n * qi
+  nn.d2 =                                 __umul32(n.d0, qi);
+  nn.d3 = __add_cc (__umul32hi(n.d0, qi), __umul32(n.d1, qi));
+  nn.d4 = __addc_cc(__umul32hi(n.d1, qi), __umul32(n.d2, qi));
+  nn.d5 = __addc   (__umul32hi(n.d2, qi),                  0);
+
+// shiftleft nn 11 bits
+  nn.d5 = (nn.d5 << 11) + (nn.d4 >> 21);
+  nn.d4 = (nn.d4 << 11) + (nn.d3 >> 21);
+  nn.d3 = (nn.d3 << 11) + (nn.d2 >> 21);
+  nn.d2 =  nn.d2 << 11;
+
+//  q = q - nn
+  q.d2 = __sub_cc (q.d2, nn.d2);
+  q.d3 = __subc_cc(q.d3, nn.d3);
+  q.d4 = __subc_cc(q.d4, nn.d4);
+  q.d5 = __subc   (q.d5, nn.d5);
+#endif // SHORTCUT_75BIT SHORTCUT_64BIT
+/********** Step 2, Offset 2^55 (1*32 + 23) **********/
+#if !defined(SHORTCUT_75BIT) && !defined(SHORTCUT_64BIT)
+  qf= __uint2float_rn(q.d5);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d4);
+#else
+  qf= __uint2float_rn(q.d4);
+#endif
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d3);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d2);
+  qf*= 512.0f;
+
+  qi=__float2uint_rz(qf*nf);
+
+  MODBASECASE_QI_ERROR(1<<22, 2, qi, 1);
+
+
+// nn = n * qi
+  nn.d1 =                                 __umul32(n.d0, qi);
+  nn.d2 = __add_cc (__umul32hi(n.d0, qi), __umul32(n.d1, qi));
+#ifndef SHORTCUT_64BIT
+  nn.d3 = __addc_cc(__umul32hi(n.d1, qi), __umul32(n.d2, qi));
+  nn.d4 = __addc   (__umul32hi(n.d2, qi),                  0);
+#else
+  nn.d3 = __addc   (__umul32hi(n.d1, qi),                  0);
+#endif
+
+// shiftleft nn 23 bits
+#ifdef DEBUG_GPU_MATH
+  nn.d5 =                  nn.d4 >> 9;
+#endif
+#ifndef SHORTCUT_64BIT
+  nn.d4 = (nn.d4 << 23) + (nn.d3 >> 9);
+#else
+  nn.d4 = (nn.d3 >> 9);
+#endif
+  nn.d3 = (nn.d3 << 23) + (nn.d2 >> 9);
+  nn.d2 = (nn.d2 << 23) + (nn.d1 >> 9);
+  nn.d1 =  nn.d1 << 23;
+
+// q = q - nn
+  q.d1 = __sub_cc (q.d1, nn.d1);
+  q.d2 = __subc_cc(q.d2, nn.d2);
+  q.d3 = __subc_cc(q.d3, nn.d3);
+#ifndef DEBUG_GPU_MATH
+  #ifndef SHORTCUT_64BIT
+  q.d4 = __subc   (q.d4, nn.d4);
+  #endif
+#else
+  q.d4 = __subc_cc(q.d4, nn.d4);
+  q.d5 = __subc   (q.d5, nn.d5);
+#endif
+
+/********** Step 3, Offset 2^35 (1*32 + 3) **********/
+  MODBASECASE_NONZERO_ERROR(q.d5, 3, 5, 2);
+
+#ifndef SHORTCUT_64BIT
+  qf= __uint2float_rn(q.d4);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d3);
+#else
+  qf= __uint2float_rn(q.d3);
+#endif
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d2);
+  qf*= 536870912.0f; // add (q.d1 >> 3) ???
+//  qf*= 4294967296.0f; /* this includes the shiftleft of qi by 3 bits! */
+
+  qi=__float2uint_rz(qf*nf);
+
+  MODBASECASE_QI_ERROR(1<<22, 3, qi, 3);
+
+// shiftleft qi 3 bits to avoid "long shiftleft" after multiplication
+  qi <<= 3;
+
+// nn = n * qi
+  nn.d1 =                                 __umul32(n.d0, qi);
+  nn.d2 = __add_cc (__umul32hi(n.d0, qi), __umul32(n.d1, qi));
+#ifndef SHORTCUT_64BIT
+  nn.d3 = __addc_cc(__umul32hi(n.d1, qi), __umul32(n.d2, qi));
+  nn.d4 = __addc   (__umul32hi(n.d2, qi),                  0);
+#else
+  nn.d3 = __addc   (__umul32hi(n.d1, qi),                  0);
+#endif
+
+//  q = q - nn
+  q.d1 = __sub_cc (q.d1, nn.d1);
+  q.d2 = __subc_cc(q.d2, nn.d2);
+  q.d3 = __subc_cc(q.d3, nn.d3);
+#ifndef SHORTCUT_64BIT
+  q.d4 = __subc   (q.d4, nn.d4);
+#endif
+
+/********** Step 4, Offset 2^15 (0*32 + 15) **********/
+  MODBASECASE_NONZERO_ERROR(q.d5, 4, 5, 4);
+
+#ifndef SHORTCUT_64BIT
+  qf= __uint2float_rn(q.d4);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d3);
+#else
+  qf= __uint2float_rn(q.d3);
+#endif
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d2);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d1);
+  qf*= 131072.0f;
+
+  qi=__float2uint_rz(qf*nf);
+
+  MODBASECASE_QI_ERROR(1<<22, 4, qi, 5);
+
+// nn = n * qi
+  nn.d0 =                                 __umul32(n.d0, qi);
+  nn.d1 = __add_cc (__umul32hi(n.d0, qi), __umul32(n.d1, qi));
+#ifndef SHORTCUT_64BIT
+  nn.d2 = __addc_cc(__umul32hi(n.d1, qi), __umul32(n.d2, qi));
+  nn.d3 = __addc   (__umul32hi(n.d2, qi),                  0);
+#else
+  nn.d2 = __addc   (__umul32hi(n.d1, qi),                  0);
+#endif
+
+// shiftleft nn 15 bits
+#ifdef DEBUG_GPU_MATH
+  nn.d4 =                  nn.d3 >> 17;
+#endif
+#ifndef SHORTCUT_64BIT
+  nn.d3 = (nn.d3 << 15) + (nn.d2 >> 17);
+#else
+  nn.d3 = (nn.d2 >> 17);
+#endif
+  nn.d2 = (nn.d2 << 15) + (nn.d1 >> 17);
+  nn.d1 = (nn.d1 << 15) + (nn.d0 >> 17);
+  nn.d0 =  nn.d0 << 15;
+
+//  q = q - nn
+  q.d0 = __sub_cc (q.d0, nn.d0);
+  q.d1 = __subc_cc(q.d1, nn.d1);
+  q.d2 = __subc_cc(q.d2, nn.d2);
+#ifndef DEBUG_GPU_MATH
+  q.d3 = __subc   (q.d3, nn.d3);
+#else
+  q.d3 = __subc_cc(q.d3, nn.d3);
+  q.d4 = __subc   (q.d4, nn.d4);
+#endif
+
+  MODBASECASE_NONZERO_ERROR(q.d5, 5, 5, 6);
+  MODBASECASE_NONZERO_ERROR(q.d4, 5, 4, 7);
+
+  qf= __uint2float_rn(q.d3);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d2);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d1);
+  qf= qf * 4294967296.0f + __uint2float_rn(q.d0);
+
+  qi=__float2uint_rz(qf*nf);
+
+  MODBASECASE_QI_ERROR(1<<20, 5, qi, 8);
+
+// nn = n * qi
+  nn.d0 =                                 __umul32(n.d0, qi);
+  nn.d1 = __add_cc (__umul32hi(n.d0, qi), __umul32(n.d1, qi));
+#ifndef DEBUG_GPU_MATH
+  #ifdef SHORTCUT_64BIT
+  nn.d2 = __addc   (__umul32hi(n.d1, qi), 0);
+  #else
+  nn.d2 = __addc   (__umul32hi(n.d1, qi), __umul32(n.d2, qi));
+  #endif
+#else
+  #ifdef SHORTCUT_64BIT
+  nn.d2 = __addc_cc(__umul32hi(n.d1, qi), 0);
+  nn.d3 = __addc   (0,                    0);
+  #else
+  nn.d2 = __addc_cc(__umul32hi(n.d1, qi), __umul32(n.d2, qi));
+  nn.d3 = __addc   (__umul32hi(n.d2, qi),                  0);
+  #endif
+#endif
+
+//  q = q - nn
+  q.d0 = __sub_cc (q.d0, nn.d0);
+  q.d1 = __subc_cc(q.d1, nn.d1);
+#ifndef DEBUG_GPU_MATH
+  q.d2 = __subc   (q.d2, nn.d2);
+#else
+  q.d2 = __subc_cc(q.d2, nn.d2);
+  q.d3 = __subc   (q.d3, nn.d3);
+#endif
+
+  res->d0=q.d0;
+  res->d1=q.d1;
+  res->d2=q.d2;
+
+  MODBASECASE_NONZERO_ERROR(q.d5, 6, 5, 9);
+  MODBASECASE_NONZERO_ERROR(q.d4, 6, 4, 10);
+  MODBASECASE_NONZERO_ERROR(q.d3, 6, 3, 11);
+
+/*
+qi is always a little bit too small, this is OK for all steps except the last
+one. Sometimes the result is a little bit bigger than n
+*/
+/*  if(cmp_ge_96(*res,n))
+  {
+    sub_96(&tmp96,*res,n);
+    copy_96(res,tmp96);
+  }*/
+}
+
+#ifndef DEBUG_GPU_MATH
 __device__ static void mod_192_96(int96 *res, int192 q, int96 n, float nf)
 #else
 __device__ static void mod_192_96(int96 *res, int192 q, int96 n, float nf, unsigned int *modbasecase_debug)
