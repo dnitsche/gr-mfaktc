73e06692b31719861aaac2acf96453cb64c8eafa
Danilo Nitsche <danilo.nitsche@gmx.de>
2024-04-24 23:54:33 +0200

    Rewrite class_needed_2

--- src/mfaktc.c
+++ src/mfaktc.c
@@ -104,7 +104,6 @@ int class_needed_default(unsigned int exp, unsigned long long int k_min, int c)
   return 0;
 }
 
-
 int class_needed_2(unsigned int exp, unsigned long long int k_min, int c)
 {
  /*
@@ -114,8 +113,9 @@ int class_needed_2(unsigned int exp, unsigned long long int k_min, int c)
 
  k_min *MUST* be aligned in that way that k_min is in class 0!
  */
-  if  ((2 * (exp %  8) * ((k_min + c) %  8)) %  8 !=  2)
-  if( ((2 * (exp %  8) * ((k_min + c) %  8)) %  8 !=  4) && \
+   int tmp;
+   tmp = (2 * (exp% 8) * ((k_min+c)% 8)) % 8;
+   if( ((tmp==0) || (tmp==6)) && \
       ((2 * (exp %  3) * ((k_min + c) %  3)) %  3 !=  2) && \
       ((2 * (exp %  5) * ((k_min + c) %  5)) %  5 !=  4) && \
       ((2 * (exp %  7) * ((k_min + c) %  7)) %  7 !=  6))
